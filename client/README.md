9300fbadb42b4aecf2b9053b7a4a6a98755a1fa2
yinfxs <yinfxs@gmail.com>
2016-10-24 23:57:36 +0800

    更新最新版本

--- client/README.md
+++
@@ -1,1 +0,0 @@
-# ibird-client
--- client/src/utils/ToastrUtils.js
+++
@@ -1,8 +0,0 @@
-/**
- * toastr工具模块
- * Created by yinfxs on 16-6-22.
- */
-
-'use strict';
-
-module.exports.defaultOptions = {progressBar: true, preventDuplicates: true};
