005c19b4bb6d03b3576cd54d3eb666efd72d1ec8
yinfxs <yinfxs@gmail.com>
2017-06-04 14:28:48 +0800

    remove html support

--- ibird-docs/index.js
+++ ibird-docs/index.js
@@ -7,9 +7,7 @@
 
 const raml = require('ibird-raml');
 const yaml = require('js-yaml');
-const raml2html = require('raml2htmlfix');
 const fs = require('fs');
-const defaultTheme = raml2html.getConfigForTheme();
 const path = require('path');
 const app = {};
 
@@ -44,34 +42,11 @@ app.build = (doc, file) => {
 };
 
 /**
- * 根据RAML生成HTML
- * @param raml RAML文件路径或RAML内容
- * @param [html] HTML文件输出路径，可选
- */
-app.html = async (raml, html) => {
-    try {
-        let result = await raml2html.render(raml, defaultTheme);
-        // 替换为国内CDN
-        result = result.replace(new RegExp(`https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css`, 'g'), 'https://cdn.bootcss.com/bootstrap/3.1.1/css/bootstrap.min.css');
-        result = result.replace(new RegExp(`https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/styles/default.min.css`, 'g'), 'https://cdn.bootcss.com/highlight.js/9.3.0/styles/default.min.css');
-        result = result.replace(new RegExp(`https://code.jquery.com/jquery-1.11.0.min.js`, 'g'), 'https://cdn.bootcss.com/jquery/1.11.0/jquery.min.js');
-        result = result.replace(new RegExp(`https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js`, 'g'), 'https://cdn.bootcss.com/bootstrap/3.1.1/js/bootstrap.min.js');
-        result = result.replace(new RegExp(`https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/highlight.min.js`, 'g'), 'https://cdn.bootcss.com/highlight.js/9.3.0/highlight.min.js');
-        if (html) fs.writeFileSync(html, result);
-        return result;
-    } catch (e) {
-        console.error(e);
-        return null;
-    }
-};
-
-/**
  * 根据ibird的配置对象生成相关文档
  * @param config ibird应用配置对象
  * @param [ramlpath] RAML文件保存路径，可选
- * @param [htmlpath] HTML文件保存路径，仅在ramlpath不为空的前提下可用
  */
-app.gen = (config, ramlpath, htmlpath) => {
+app.gen = (config, ramlpath) => {
     if (!config) return;
     const doc = {
         title: config.name,
@@ -84,5 +59,5 @@ app.gen = (config, ramlpath, htmlpath) => {
     raml.routeApis(doc, config);
 
     const result = app.build(doc, ramlpath);
-    return ramlpath && htmlpath ? app.html(ramlpath, htmlpath) : result;
+    return result;
 };
