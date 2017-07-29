276229a2326db66641107e8ad99020fab10ef115
yinfxs <yinfxs@gmail.com>
2017-07-29 17:22:54 +0800

    Compatible string style of the sort parameter

--- ibird-core/lib/route/list.js
+++ ibird-core/lib/route/list.js
@@ -4,6 +4,29 @@ const hooks = require('../hooks');
 const common = require('../common');
 
 /**
+ * 将排序字符串格式转换成对象格式
+ * @param sortStr 排序字符串
+ */
+function sortStr2Obj(sortStr) {
+    if (!sortStr || !sortStr.trim()) return {};
+    const sorts = sortStr.trim().split(' ');
+    const obj = {};
+    if (sorts.length === 0) return {};
+    for (let key of sorts) {
+        if (!key) continue;
+        key = key.trim();
+        let order = 1;
+        if (key.startsWith('-')) {
+            //逆序
+            key = key.substr(1);
+            order = -1;
+        }
+        obj[key] = order;
+    }
+    return obj;
+}
+
+/**
  * 默认列表查询路由
  * @param name 模型名称
  * @param [pre] 前置处理函数（数组）
@@ -22,10 +45,11 @@ module.exports = (name, pre, post) => {
         _query.range = _query.range ? _query.range.toUpperCase() : _PAGE;
         _query.sort = _query.sort || '_id';
 
-        const _sort = utility.parse(_query.sort);
+        let _sort = utility.parse(_query.sort);
+        _sort = Object.keys(_sort).length > 0 ? _sort : sortStr2Obj(_query.sort);
         const _range = ctx._range || {};
 
-        _query.sort = Object.keys(_sort).length > 0 ? _sort : _query.sort;
+        _query.sort = _sort;
         _query.cond = utility.parse(_query.cond);
         _query.page = !Number.isNaN(_query.page) && _query.page > 0 ? _query.page : 1;
         _query.size = !Number.isNaN(_query.size) && _query.size > 0 ? _query.size : 20;
