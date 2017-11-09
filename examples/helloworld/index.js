0f679589912db5a40badefab5770644e6c1eb6cf
yinfxs <yinfxs@gmail.com>
2017-11-09 22:43:07 +0800

    add 'newApp' exports

--- examples/helloworld/index.js
+++ examples/helloworld/index.js
@@ -3,11 +3,6 @@
  * @type {App}
  */
 
-const App = require('../../lib/index').App;
-const app = new App();
-
-// 挂载自定义路由
-app.get('/hello', ctx => ctx.body = `Hello ibird（${new Date().toLocaleString()}）!`);
-
-// 启动应用：默认3000端口
+const app = require('../../lib/index').newApp();
+app.get('/', ctx => ctx.body = `Hello ibird.`);
 app.play();
